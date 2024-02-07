508b915b8374fe1f4cf14e2015daa6fd98f28eb1
Vadimych1 <vadimlebdenko0@gmail.com>
2024-02-07 21:58:20 +0300

    module server add

--- Parser/Parser.py
+++ Parser/Parser.py
@@ -1,10 +1,9 @@
-
 import requests
 from bs4 import BeautifulSoup
-url = requests.get("https://bolshayastrana.com/regiony")
-soup = BeautifulSoup(url.content, "html.parser")
-balances = soup.find_all('div', {"class": "region-card__body-title"})
-for balance in balances:
-    print(balance.text)
 
-
+def main(request):
+    url = requests.get("https://bolshayastrana.com/regiony")
+    soup = BeautifulSoup(url.content, "html.parser")
+    balances = soup.find_all('div', {"class": "region-card__body-title"})
+    for balance in balances:
+        print(balance.text)
---
+++ Parser/send_module.py
@@ -0,0 +1,23 @@
+import http.server as hts
+import Parser as p
+
+MODULE_PORT = 1000
+
+def process(self: hts.BaseHTTPRequestHandler) -> str:
+    # Process data here
+    p.main(self.rfile.read(int(self.headers['Content-Length'])))
+
+class Handler(hts.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+
+        # HERE THE REQUEST PROCESSING
+        resp = process(self)
+
+        self.wfile.write(resp.encode())
+
+httpd = hts.HTTPServer(('localhost', MODULE_PORT), Handler)
+print("::LOG:: Serving at port", MODULE_PORT)
+httpd.serve_forever()
