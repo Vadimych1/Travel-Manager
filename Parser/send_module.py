a8edc0834eb1bb9ae7bd1b50ebe13a0c006f8d21
Vadimych1 <vadimlebdenko0@gmail.com>
2024-02-08 10:49:52 +0300

    service

--- Parser/send_module.py
+++ Parser/send_module.py
@@ -5,9 +5,7 @@ MODULE_PORT = 1000
 
 def process(self: hts.BaseHTTPRequestHandler) -> str:
     # Process data here
-    p.main(self.rfile.read(int(self.headers['Content-Length'])))
-
-    data = ""
+    data = p.main(self.rfile.read(int(self.headers['Content-Length'])))
 
     return data
 
@@ -17,7 +15,6 @@ class Handler(hts.BaseHTTPRequestHandler):
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        # HERE THE REQUEST PROCESSING
         resp = process(self)
 
         self.wfile.write(resp.encode())
